01559aebb8f2db103e7b79e8a0f7663877b36f22
shooyaaa <shooyaaa@gmail.com>
2017-06-01 16:56:26 +0800

    add get bytes

--- bitmap.c
+++ bitmap.c
@@ -11,6 +11,7 @@ static ZEND_METHOD(bitmap, setOption);
 static ZEND_METHOD(bitmap, setBit);
 static ZEND_METHOD(bitmap, getBit);
 static ZEND_METHOD(bitmap, dumpBit);
+static ZEND_METHOD(bitmap, getBytes);
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_bitmap_base___construct, 0, 0, 0)
     ZEND_ARG_INFO(0, opt)
@@ -34,12 +35,16 @@ ZEND_BEGIN_ARG_INFO_EX(arginfo_bitmap_base_setBit, 0, 0, 2)
     ZEND_ARG_INFO(0, value)
 ZEND_END_ARG_INFO()
 
+ZEND_BEGIN_ARG_INFO_EX(arginfo_bitmap_base_getBytes, 0, 0, 0)
+ZEND_END_ARG_INFO()
+
 static zend_function_entry bitmap_base_methods[] = {
     ZEND_ME(bitmap, __construct, arginfo_bitmap_base___construct, ZEND_ACC_PUBLIC)
     ZEND_ME(bitmap, setOption, arginfo_bitmap_base_setOption, ZEND_ACC_PUBLIC)
     ZEND_ME(bitmap, setBit, arginfo_bitmap_base_setBit, ZEND_ACC_PUBLIC)
     ZEND_ME(bitmap, getBit, arginfo_bitmap_base_getBit, ZEND_ACC_PUBLIC)
     ZEND_ME(bitmap, dumpBit, arginfo_bitmap_base_dumpBit, ZEND_ACC_PUBLIC)
+    ZEND_ME(bitmap, getBytes, arginfo_bitmap_base_getBytes, ZEND_ACC_PUBLIC)
     {NULL, NULL, NULL}
 };
 
@@ -163,6 +168,12 @@ static ZEND_METHOD(bitmap, dumpBit) {
     }
     RETVAL_STRINGL(result, total);
     efree(result);
+}
+
+static ZEND_METHOD(bitmap, getBytes) {
+    php_bitmap_base_t *intern;
+    Z_BITMAP_INTERN(intern, Z_OBJ_P(getThis()));
+    RETURN_STRINGL(intern->bytes, intern->size);
 }
 
 static ZEND_METHOD(bitmap, __construct) /* {{{ */ {
